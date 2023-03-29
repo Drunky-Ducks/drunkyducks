c4707aa95d7ecbc4a691a7ef83a7025e7d061c7c
Juan Felipe Ramirez <juanfel2024@gmail.com>
2023-03-29 20:19:36 +0200

    Creado el componente CardMinimal

---
+++ src/components/CardMinimal.vue
@@ -0,0 +1,85 @@
+<script>
+export default {
+  name: "CardMinimal",
+  props: {
+    cocktail: Object
+  },
+  methods: {
+    getAlt: () => {
+      return `cocktail ${this.cocktail.strDrink.toLowerCase()} image`
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <img :src="cocktail.strDrinkThumb" :alt="getAlt">
+    <div>
+      <h1>{{ cocktail.strDrink }}</h1>
+      <span>ℹ</span>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  text-align: center;
+  filter: drop-shadow(2px 2px 5px black) drop-shadow(-2px -2px 5px black)
+}
+
+img {
+  position: relative;
+  bottom: -15px;
+  width: 80%;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  border: 5px solid #40bfc1;
+  box-shadow: 2px 2px 5px black, -2px -2px 5px black;
+}
+
+div {
+  width: 80%;
+  background: #202124;
+  border: 5px solid #40bfc1;
+  color: #FFC300;
+  border-radius: 7px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  padding: 20px;
+}
+
+h1 {
+  cursor: pointer;
+}
+
+span {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  background-color: #40bfc1;
+  /* Color de fondo */
+  color: black;
+  /* Color del texto */
+  padding: 10px;
+  /* Espacio interno del botón */
+  border: 1px solid black;
+  /* Sin borde */
+  border-radius: 50%;
+  /* Bordes redondeados */
+  cursor: pointer;
+  /* Cambia el cursor al pasar el ratón */
+  font-size: 1.3rem;
+  font-weight: bold;
+}
+</style>
