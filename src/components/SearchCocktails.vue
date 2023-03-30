a175f487d14522ecb584ee248c3acfa7e03e6ba6
sisabel <sisabel2524@gmail.com>
2023-03-30 13:36:07 +0100

    Buscador
    Se creo el componete que contiene el buscador de cocteles por nombre

---
+++ src/components/SearchCocktails.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'SeacrhCocktails',
+  props: {
+    cocktail: String
+  },
+  data() {
+    return {
+      search: ''
+    }
+  },
+  computed: {
+    nameCocktails({data}) {
+      return data.filter((nameCocktail) => {
+        return nameCocktail.strDrink.toLowerCase().includes(this.search.toLowerCase())
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="search-container">
+    <input type="search" v-model="search" placeholder="Mojito" />
+  </div>
+</template>
+
+<style></style>
