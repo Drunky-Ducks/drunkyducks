e6b675196b1cf0d842a473128c6f79f8b04a7ae3
Juan Antonio Fernandez <juananfernandez6@gmail.com>
2023-04-12 12:17:10 +0200

    Añadido la libreria Swiper con sus categorias y cocteles

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,8 +1,7 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <h1>{{ this.title }}</h1>
-  <swiper :slidesPerView="3" :spaceBetween="30" :freeMode="true" :pagination="{
-    clickable: true,
-  }" :modules="modules" class="mySwiper">
+  <swiper :slidesPerView="4" :spaceBetween="30" :navigation="true" :modules="modules" :loop="true" class="mySwiper">
     <swiper-slide v-for="(cocktail, index) in cocktails" :key="index">
       <CardMinimal :cocktail="cocktail"></CardMinimal>
     </swiper-slide>
@@ -14,12 +13,11 @@ import { Swiper, SwiperSlide } from 'swiper/vue';
 import CardMinimal from '../components/CardMinimal.vue';
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
 
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
 
 // import required modules
-import { FreeMode, Pagination } from 'swiper';
+import { Navigation } from 'swiper';
 
 export default {
   components: {
@@ -27,10 +25,11 @@ export default {
   },
   setup() {
     return {
-      modules: [FreeMode, Pagination],
+      modules: [Navigation],
     };
   },
   props: {
+    // eslint-disable-next-line vue/require-prop-type-constructor
     title: "",
     cocktails: [],
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script>
 import cocktails from '../services/cocktails.json'
+// eslint-disable-next-line no-unused-vars
 import CocktailDetails from '../components/CocktailDetails.vue';
 import Swiper from '../components/Swiper.vue';
+// eslint-disable-next-line no-unused-vars
+import Swipercss from "../assets/swiper.css";
+
 
 export default {
   name: "HomeView",
@@ -18,11 +22,66 @@ export default {
 </script>
 
 <template>
-  <Swiper title="pukytuki" :cocktails="this.cocktails.drinks.filter(cocktail => cocktail.strClassic)"></Swiper>
-  <Swiper title="pukytuki2" :cocktails="this.cocktails.drinks.filter(cocktail => cocktail.str)"></Swiper>
-  <Swiper title="pukytuki3" :cocktails="this.cocktails.drinks.filter(cocktail => cocktail.str)"></Swiper>
-  <!-- FALTA LA URL DEL STR LO QUE -->
+  <div class="container">
+    <div class="title">
+      <Swiper title="Cocteles Clasicos"></Swiper>
+    </div>
+    <div class="cocktails">
+      <Swiper :cocktails="this.cocktails.drinks.filter(cocktail => cocktail.strClassic)"></Swiper>
+    </div>
+  </div>
+  <div class="container">
+    <div class="title">
+      <Swiper title="Cocteles Sin Alcohol"></Swiper>
+    </div>
+    <div class="cocktails">
+      <Swiper :cocktails="this.cocktails.drinks.filter(cocktail => cocktail.strNoAlcoholic)"></Swiper>
+    </div>
+  </div>
+  <div class="container">
+    <div class="title">
+      <Swiper title="Cocteles Fuertes"></Swiper>
+    </div>
+    <div class="cocktails">
+      <Swiper :cocktails="this.cocktails.drinks.filter(cocktail => cocktail.strHardDrink)"></Swiper>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+@media screen and (max-width: 320px) {
+  .swiper {
+    slidesPerView: 2,
+    spaceBetween: 20
+  }
+
+  .info {
+    font-size: 12px;
+    width: auto;
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .swiper {
+    slidesPerView: 3,
+    spaceBetween: 30
+  }
+
+  .info {
+    font-size: 12px;
+    width: auto;
+  }
+}
+
+@media screen and (max-width: 640px) {
+  .swiper {
+    slidesPerView: 4,
+    spaceBetween: 40
+  }
+
+  .info {
+    font-size: 12px;
+    width: auto;
+  }
+}
 </style>
