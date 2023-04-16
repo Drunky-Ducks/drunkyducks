4473eb13f8f4963928d3955165773d2e6ddc699c
Eduardo Diaz <edudug98@gmail.com>
2023-04-16 13:40:19 +0100

    Crear vista del formulario para un cóctel nuevo
    Se creo la vista del formulario para que el
    usuario pueda añadir un nuevo cóctel. También
    se añadió la ruta para acceder a esa vista.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-
-
 import GamesView from '../views/GamesView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,10 +19,16 @@ const router = createRouter({
       path: '/details/:id',
       name: 'details',
       component: () => import('../views/DetailsView.vue')
-    },{
+    },
+    {
       path: '/search',
       name: 'search',
       component: () => import('../views/SearchView.vue')
+    },
+    {
+      path: '/form',
+      name: 'form',
+      component: () => import('../views/FormView.vue')
     }
   ]
 })
---
+++ src/views/FormView.vue
@@ -0,0 +1,118 @@
+<script>
+export default {
+    name: 'NewCocktailForm'
+}
+</script>
+<template>
+    <form action="https://formsubmit.co/drunkyducks@gmail.com" method="POST">
+        <h1>Crea tu propio cóctel</h1>
+
+        <div class="cocktail-details">
+            <input type="email" name="email" class="email" placeholder="Correo electrónico" required>
+            <input type="cocktail" name="cocktail" class="cocktail" placeholder="Nombre del cóctel" required>
+            <textarea name="description" class="description" placeholder="Descripción o ingredientes del cóctel"></textarea>
+        </div>
+
+        <button type="submit">ENVIAR</button>
+
+        <!-- <input type="hidden" name="_next" value="PONER DOMINIO DE DRUNKY DUCKS"> -->
+        <input type="hidden" name="_captcha" value="false">
+    </form>
+</template>
+
+<style scoped>
+* {
+    /* Fuente provisional, borrar */
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+body {
+    margin: 0;
+}
+
+form {
+    height: 100vh;
+    background-color: #292929;
+    padding: 20px;
+    display: grid;
+    grid-template-rows: 1fr 1fr 1.2fr;
+    gap: 20px;
+}
+
+h1 {
+    color: #FFC300;
+    align-self: center;
+    text-align: center;
+    font-weight: 700;
+    font-size: 45px;
+    text-shadow: 0 0 10px black;
+}
+
+.cocktail-details {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+input,
+textarea {
+    padding: 20px;
+    background-color: #f5f5f5;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    box-shadow: 0 0 5px #f5f5f5;
+    ;
+}
+
+textarea {
+    resize: none;
+    height: 250px;
+}
+
+form button {
+    width: 150px;
+    padding: 15px;
+    background-color: #FFC300;
+    color: #5f5f5f;
+    border: none;
+    border-radius: 15px;
+    outline: none;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: 700;
+    align-self: flex-start;
+    justify-self: center;
+}
+
+form button:active {
+    box-shadow: 0 0 10px #FFC300;
+    transform: scale(0.95);
+    transition: all 0.2s ease-out;
+}
+
+input:hover,
+textarea:hover {
+    background-color: #e4e4e4f5;
+    transition: all 0.7s ease-in-out;
+}
+
+@media screen and (min-width: 425px) {
+    body {
+        background-color: #292929;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    textarea {
+        height: 150px;
+    }
+
+    form {
+        width: 80%;
+    }
+}
+</style>
