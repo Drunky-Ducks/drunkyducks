c5168a6fb887d7f1be576078a348ebf3cf31a9e8
sisabel <sisabel2524@gmail.com>
2023-04-18 23:22:50 +0100

    rediseño de la card con los detalles del coctel seleccionado

--- src/components/CocktailDetails.vue
+++ src/components/CocktailDetails.vue
@@ -1,12 +1,12 @@
 <script>
 export default {
-  name: "CocktailDetails",
+  name: 'CocktailDetails',
   props: {
     cocktail: Object
   },
   methods: {
     getAlt: function () {
-      return `cocktail ${this.cocktail.strDrink.toLowerCase()} image`;
+      return `cocktail ${this.cocktail.strDrink.toLowerCase()} image`
     }
   }
 }
@@ -14,24 +14,27 @@ export default {
 
 <template>
   <div class="container">
-    <div>
-      <h1>{{ cocktail.strDrink }}</h1>
-      <h2>{{ cocktail.strAlcoholic }}</h2>
-    </div>
     <picture>
-      <img :src="cocktail.strDrinkThumb" :alt="getAlt()">
+      <img :src="cocktail.strDrinkThumb" :alt="getAlt()" />
     </picture>
-    <div class="ingredients-container">
-      <h3>Ingredientes:</h3>
-      <ul class="ingredients">
-        <li v-for="(value, index) in cocktail.strIngredients" :key="index">
-          {{ value }} <span v-if="cocktail.strMeasures[index]"> - {{ cocktail.strMeasures[index] }} </span>
-        </li>
-      </ul>
-    </div>
-    <div>
-      <h3>Intrucciones:</h3>
-      <p>{{ cocktail.strInstructions }}</p>
+    <div class="info-cocktail">
+      <div class="drink">
+        <h1>{{ cocktail.strDrink }}</h1>
+        <h2>{{ cocktail.strAlcoholic }}</h2>
+      </div>
+      <div class="ingredients-container">
+        <h3>Ingredientes</h3>
+        <ul class="ingredients">
+          <li v-for="(value, index) in cocktail.strIngredients" :key="index">
+            {{ value }}
+            <span v-if="cocktail.strMeasures[index]"> - {{ cocktail.strMeasures[index] }} </span>
+          </li>
+        </ul>
+      </div>
+      <div class="instructions">
+        <h3>Instrucciones</h3>
+        <p>{{ cocktail.strInstructions }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -39,33 +42,31 @@ export default {
 <style scoped>
 .container {
   width: 100%;
-  max-width: 500px;
-  border: 1px solid black;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
   padding: 10px;
-  gap: 30px;
-  border-top: 5px solid #ffc300;
-  border-bottom: 5px solid #ffc300;
+  border-top: 3px solid #40bfc1;
+  border-bottom: 3px solid #ffc300;
   box-shadow: #00000082 0 15px 30px;
+  padding: 3rem 0;
 }
 
 h1 {
-  font-size: 4rem;
-  color: #FFC300;
+  font-size: 3.5rem;
+  color: #ffc300;
   margin-bottom: 10px;
+  font-family: DynaPuff-Regular;
 }
 
 h2 {
-  font-size: 1.5rem;
+  font-size: 1.3rem;
 }
 
 h3 {
-  color: #FFC300;
-  font-size: 1.3rem;
+  color: #40bfc1;
+  font-size: 1.8rem;
   margin-bottom: 10px;
+  font-family: DynaPuff-Regular;
 }
 
 h1,
@@ -74,14 +75,11 @@ h3 {
   text-align: center;
 }
 
-picture {
-  width: 20rem;
-  height: 15rem;
-}
-
 img {
   width: 100%;
   height: 100%;
+  border-radius: 15px;
+  box-shadow: 0px 0px 50px 0px rgba(0, 0, 0, 0.64);
 }
 
 ul {
@@ -90,6 +88,40 @@ ul {
 }
 
 p {
-  text-indent: 15px;
+  text-align: center;
+}
+.info-cocktail {
+  display: flex;
+  flex-direction: column;
+  font-size: 1.3rem;
+  gap: 2rem;
+}
+
+@media screen and (min-width: 1000px) {
+  .container {
+    justify-content: center;
+    width: 80%;
+    min-height: 30rem;
+  }
+
+  picture {
+    width: 30%;
+  }
+  .info-cocktail {
+    width: 50%;
+  }
+}
+
+@media screen and (max-width: 999px) {
+  .container {
+    max-width: 500px;
+    flex-direction: column;
+    justify-content: center;
+    gap: 30px;
+    padding: 3rem;
+  }
+  picture {
+    width: 70%;
+  }
 }
 </style>
